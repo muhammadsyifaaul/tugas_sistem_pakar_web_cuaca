883e24630f8eab4120d1f38f831c9da963fce899
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-09-08 13:14:13 +0700

    loader for login page

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,11 +1,21 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+// const container = document.getElementById('container');
+// const registerBtn = document.getElementById('register');
+// const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add("active");
+// });
+
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove("active");
+// });
+
+
+const preLoader = document.querySelector('.splash');
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        preLoader.classList.add('hidden');
+    }, 2000);
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
