6659f0fb9fd8e6d27764ebfb3ad7cc3f1f09ea51
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-09-11 00:42:29 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,37 @@
 const { fetchData } = require('../api/getData');
 const { getIp } = require('../api/getLocation');
+
+const objCondition = {
+    'Sunny': 'bx bxs-sun',
+    'Clear': 'bx bxs-sun',
+    'Partly Cloudy': 'bx bxs-cloud',
+    'Partly cloudy': 'bx bxs-cloud',
+    'Cloudy': 'bx bxs-cloud',
+    'Patchy light rain': 'bx bxs-cloud-rain',
+    'Overcast': 'bx bxs-sun',
+    'Mist': 'bx bxs-sun',
+    'Patchy rain possible': 'bx bxs-cloud-rain',
+    'Light rain': 'bx bxs-cloud-rain',
+    'Moderate rain at times': 'bx bxs-cloud-rain',
+    'Moderate rain': 'bx bxs-cloud-rain',
+    'Heavy rain at times': 'bx bxs-cloud-rain',
+    'Heavy rain': 'bx bxs-cloud-rain',
+    'Moderate or heavy rain with thunder': 'bx bx-cloud-lightning',
+    'Patchy light rain with thunder': 'bx bx-cloud-rain',
+    'Thundery outbreaks possible': 'bx bxs-cloud-lightning',
+    'Ice pellets': 'bx bx-cloud-snow'
+}
+
 exports.homePage = async (req, res) => {
     try {
         const location =  await getIp();
         const data = await fetchData(location);
-        // res.cookies('condition',`${data.current.condition.text}`)
+        const condition = data.current.condition.text
+        for(let i in objCondition) {
+            if(i === condition) {
+                data.current.condition.icon = objCondition[i]
+            }
+        }
         res.render('main/index', { data });
         console.log(data)
     } catch (err) {
@@ -15,7 +42,12 @@ exports.homePage = async (req, res) => {
 exports.search = async (req,res) => {
     const {location} = req.query
     const data = await fetchData(location)
-    // res.cookies('condition',`${data.current.condition.text}`)
+    const condition = data.current.condition.text
+        for(let i in objCondition) {
+            if(i === condition) {
+                data.current.condition.icon = objCondition[i]
+            }
+        }
     console.log(data)
     res.render('main/index', {data})
 }
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,16 +1,3 @@
-// const container = document.getElementById('container');
-// const registerBtn = document.getElementById('register');
-// const loginBtn = document.getElementById('login');
-
-// registerBtn.addEventListener('click', () => {
-//     container.classList.add("active");
-// });
-
-// loginBtn.addEventListener('click', () => {
-//     container.classList.remove("active");
-// });
-
-
 const preLoader = document.querySelector('.splash');
 
 window.addEventListener('load', () => {
@@ -19,5 +6,11 @@ window.addEventListener('load', () => {
         preLoader.style.pointerEvents = 'none';
     }, 3000);
 });
+
+const icon = document.querySelector('.condition i') 
+const classValue = icon.getAttribute('class');
+if(classValue.includes('sun')) {
+    icon.style.animation = 'rotateSun 5s linear infinite';
+}
 
 
